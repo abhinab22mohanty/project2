63964235ad842f64bfbcb1ff573c39684eae0256
Abhinab <amohant7@asu.edu>
2019-03-04 11:36:31 -0700

    Adding project

--- Team_47/QuizCreator/WebContent/js/myquizzes.js
+++
@@ -1,9 +0,0 @@
-
-var queryString = decodeURIComponent(window.location.search);
-queryString = queryString.substring(1);
-var queries = queryString.split("&");
-// Not working now....
-for (var i = 0; i < queries.length; i++) {
-//  alert(queries[i]);
-}
-
